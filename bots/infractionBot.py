d6e1fe7052c43705cc6aed04bd3c8c66d2eea430
Keshav Gupta <info@keshlabs.in>
2019-02-19 21:13:57 -0500

    LMAO OKAY

--- bots/infractionBot.py
+++ bots/infractionBot.py
@@ -2,54 +2,52 @@
 
 from fbchat import Client
 from fbchat.models import *
-
-infractionsList = open("infractionsList.txt")
-infractionsDict = {}
+import json
 
 thread_id = "1599716096804784"
 thread_type = ThreadType.GROUP
 
-# Read infractionList file
-for line in infractionsList:
-    line = line.split(":")
-    infractionsDict[line[0]] = int(line[1].strip())
+with open("infractions.json") as infractionsDB:
+    infractions = json.loads(infractionsDB.read())
 
-infractionsList.close()
-
-
-def writeInfractionsDict(dictionary):
-    infractionsList = open("infractionsList.txt", "w")
-    for chatter in dictionary:
-        infractionsList.write(chatter + ": " + str(dictionary[chatter]) + "\n")
-    infractionsList.close()
+def writeInfractionsToFile():
+    with open("infractions.json", "w") as js:
+        js.write(json.dumps(infractions))
 
 class InfractionClient(Client):
     def onMessage(self, mid, author_id, message_object, thread_id, thread_type, ts, metadata, msg, **kwargs):
-        # Do something with message_object here
         if "++" in message_object.text:
-            for chatter in infractionsDict:
+            for chatter in infractions:
                 if chatter.lower() in message_object.text.lower():
-                    infractionsDict[chatter] += 1
-
-            client.send(Message(text=str(infractionsDict)), thread_id=thread_id, thread_type=thread_type)
-
+                    infractions[chatter] += 1
+            client.send(Message(text=str(infractions)), thread_id=thread_id, thread_type=thread_type)
             worstChatter = ""
             worstScore = 0
-            for chatter in infractionsDict:
-                if infractionsDict[chatter] > worstScore:
-                    worstScore = infractionsDict[chatter]
+            for chatter in infractions:
+                if infractions[chatter] > worstScore:
+                    worstScore = infractions[chatter]
                     worstChatter = chatter
-
-            client.send(Message(text="Fuck you " + worstChatter), thread_id=thread_id, thread_type=thread_type)
-            writeInfractionsDict(infractionsDict)
-
-        elif "-----" in message_object.text:
-            for chatter in infractionsDict:
+            client.send(Message(text="Fuck " + worstChatter), thread_id=thread_id, thread_type=thread_type)
+            writeInfractionsToFile()
+        elif "--" in message_object.text:
+            for chatter in infractions:
                 if chatter.lower() in message_object.text.lower():
-                    infractionsDict[chatter] -= 1
-
-            client.send(Message(text=str(infractionsDict)), thread_id=thread_id, thread_type=thread_type)
-            writeInfractionsDict(infractionsDict)
+                    infractions[chatter] -= 1
+            client.send(Message(text=str(infractions)), thread_id=thread_id, thread_type=thread_type)
+            writeInfractionsToFile()
+        elif ("infraction" in message_object.text or "Infraction" in message_object.text) and str(author_id) != "100033965962382":
+            client.send(Message(text="I like it when my friends remember me :)"), thread_id=thread_id, thread_type=thread_type)
+        elif ("guideline" in message_object.text or "Guideline" in message_object.text) and str(author_id) != "100033965962382":
+            client.send(Message(text="""*community guidelines*
+                    - First and foremost, fair usage applies so no spamming. 
+                    - Be discretionary while giving strikes. Remember, power is linearly correlated with responsibility - Spiderman 
+                    - Remove strikes if the other person does something nice."""), thread_id=thread_id, thread_type=thread_type)
+        elif "rip" in message_object.text or "RIP" in message_object.text:
+            client.send(Message(text="F"), thread_id=thread_id, thread_type=thread_type)
+        elif ("sad" in message_object.text or "SAD" in message_object.text) and str(author_id) != "100033965962382":
+            client.send(Message(text="this is so sad alexa play despacito\nhttps://www.youtube.com/watch?v=kJQP7kiw5Fk"), thread_id=thread_id, thread_type=thread_type)
+        elif "👀" in message_object.text and str(author_id) != "100033965962382":
+            client.send(Message(text="👀"), thread_id=thread_id, thread_type=thread_type)
 
 client = InfractionClient("neuralnetmemes@gmail.com", "infractionBot")
 client.listen()
